47ae8d16ab787fe84d995b8fe408cf92366d9380
ahua <2102413544@qq.com>
2020-07-09 15:33:17 +0800

    开发项目创建

---
+++ src/main/java/com/tzl/tzlcdp/TzlcdpApplication.java
@@ -0,0 +1,13 @@
+package com.tzl.tzlcdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzlcdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TzlcdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tzl/tzlcdp/TzlcdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzl.tzlcdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TzlcdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
