8f47896153f79058382f17d41559bb050bb2fac8
ahua <2102413544@qq.com>
2020-07-09 16:51:07 +0800

    controller 测试

--- hrms/src/main/java/com/tzl/hrms/controller/TestController.java
+++ hrms/src/main/java/com/tzl/hrms/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.tzl.hrms.controller;
 
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+
 /**
  * @author 木头
  * @date 2020/7/9 16:43
  */
+@Controller
 public class TestController {
 
 }
